3ffd210cd2f25a144cea8976528022cea497f470
syedsimanta03 <syed.simanta06@gmail.com>
2022-11-26 15:36:21 +0600

    without hook

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RandomizerPage extends StatefulWidget {
   final int min, max;
-  
+
   const RandomizerPage({
     Key? key,
     required this.min,
@@ -15,6 +17,9 @@ class RandomizerPage extends StatefulWidget {
 }
 
 class _RandomizerPageState extends State<RandomizerPage> {
+  int? _generatedNumber;
+  final randomGenerator = Random();
+
   @override
   Widget build(BuildContext context) {
     widget.min;
@@ -22,6 +27,22 @@ class _RandomizerPageState extends State<RandomizerPage> {
       appBar: AppBar(
         title: const Text('Randomizer'),
       ),
+      body: Center(
+        child: Text(
+          _generatedNumber?.toString() ?? "Generate a number",
+          style: const TextStyle(fontSize: 30 ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Generate'),
+        onPressed: () {
+          setState(() {
+            _generatedNumber = widget.min +
+                randomGenerator.nextInt(widget.max + 1 - widget.min);
+          });
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
